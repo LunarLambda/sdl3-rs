09f4f04131fda978d2d47e75e9d97f86762fefda
LunarLambda <lunarlambda@gmail.com>
2023-08-21 14:32:06 +0200

    [WIP] prettify the generated bindings a little more, clean up build.rs

--- sdl3-sys/build.rs
+++ sdl3-sys/build.rs
@@ -1,46 +1,44 @@
-#[cfg(windows)]
-// Bindgen/clang does not do path normalization, so we end up with stuff like
-// <some prefix>\sdl/include\SDL3/<some header>
-//
-// TODO: Seems like it would generally be preferred to ship pregenerated bindings per-platform
-// Since this A: gets rid of the ever-fickle bindgen dependency, as well as needing extra library
-// headers for SDL(?), as well as letting us say "these bindings target version <x>" rather than
-// just being whatever the downstream user has available.
-const REGEX: &str = r#"(.*[/\\])?sdl[/\\]include[/\\]SDL3[/\\].*"#;
-
-#[cfg(not(windows))]
-const REGEX: &str = r#"(.*/)?sdl/include/SDL3/.*"#;
-
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    let mut build = cmake::Config::new("sdl");
+    cmake_build();
+    run_bindgen();
+}
 
-    build
-    .define("SDL_DISABLE_INSTALL_DOCS", "ON")
-    .define("SDL_SHARED", "ON")
-    .define("SDL_STATIC", "OFF") // SDL discourages static linking (see docs/README-dynapi.md)
-                                 // and pre-built binaries for Windows are only available as DLLs.
-                                 // I know DLL search paths esp. in Rust are a pain, but maintaining
-                                 // another binary build configuration is an even bigger pain.
-    .generator("Ninja");         // A valiant effort at speeding up builds a little.
 
-    let dst = build.build();
+fn cmake_build() {
+    let dst = cmake::Config::new("sdl")
+        .define("SDL_DISABLE_INSTALL_DOCS", "ON")
+        .define("SDL_TEST_LIBRARY", "OFF")
+        .generator("Ninja")
+        .build();
 
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=dylib=SDL3");
+}
 
-    let _bindgen = bindgen::builder()
-        .allowlist_file(REGEX)
-        .clang_arg("-Isdl/include")
-        // Bindgen chokes on _Float16 _Complex definitions in clang's intrinsics headers.
-        // Disable them via preprocessor.
-        .clang_arg("-D__AVX512VLFP16INTRIN_H")
-        .clang_arg("-D__AVX512FP16INTRIN_H")
+// FIXME: replace with pre-generated bindings. This is possible because the SDL3 headers have no
+// build-time logic, just per-platform logic, so it's relatively straightforward to generate
+// bindings for every platform and shove them in src/.
+fn run_bindgen() {
+    bindgen::builder()
+        .allowlist_file(r#"(.*[/\\])?SDL3[/\\].*"#)
+        .blocklist_file(r#"(.*[/\\])?SDL_platform_defines.h"#) // Don't need these
+        .blocklist_type("SDL_DUMMY_ENUM")
         // SDL's doc comments are inconsistent and don't translate to
         // rustdoc very well and cause things like spurious warnings and
         // build failures.
         .generate_comments(false)
+        .generate_cstr(true)
+        .opaque_type("va_list")
+        .prepend_enum_name(false)
+        .clang_arg("-Isdl/include")
+        // Bindgen chokes on _Float16 _Complex definitions in clang's intrinsics headers.
+        // Disable them via preprocessor.
+        .clang_arg("-D__AVX512FP16INTRIN_H")
+        .clang_arg("-D__AVX512VLFP16INTRIN_H")
+        // SDL.h includes everything we want except syswm and vulkan. The rest are expositional
+        // headers and bundled OpenGL stuff we don't care about.
         .header("sdl/include/SDL3/SDL.h")
         .header("sdl/include/SDL3/SDL_syswm.h")
         .header("sdl/include/SDL3/SDL_vulkan.h")
