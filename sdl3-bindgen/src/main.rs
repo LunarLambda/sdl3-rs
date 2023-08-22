52ee663b7137638d2926c5fb19fcc40cd244328e
LunarLambda <lunarlambda@gmail.com>
2023-08-22 14:34:20 +0200

    clean up sdl3-bindgen

--- sdl3-bindgen/src/main.rs
+++ sdl3-bindgen/src/main.rs
@@ -1,24 +1,43 @@
 fn main() {
     bindgen::builder()
-        .allowlist_file(r#"(.*[/\\])?SDL3[/\\].*"#)
-        .blocklist_file(r#"(.*[/\\])?SDL_platform_defines.h"#) // Don't need these
-        .blocklist_type("SDL_DUMMY_ENUM")
-        // Drop the va_list stuff because it's not useful in current Rust
-        .blocklist_type(".*va_list.*")
-        .blocklist_function("SDL_LogMessageV")
-        .blocklist_function(r#"SDL_v\w+(scanf|printf)"#)
-        // SDL's doc comments are inconsistent and don't translate to
-        // rustdoc very well and cause things like spurious warnings and
-        // build failures.
-        .generate_comments(false)
-        .generate_cstr(true)
-        .prepend_enum_name(false)
-        .sort_semantically(true)
-        .clang_arg("-Iheaders")
-        .header("wrapper.h")
-        // .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .unwrap()
-        .write_to_file("bindings.rs")
-        .unwrap();
+
+    // allow only the SDL3 headers to avoid unnecessary libc definitions
+    .allowlist_file(r#"(.*[/\\])?SDL3[/\\].*\.h"#)
+
+    // only used for platform detection in C. use cfg(...) instead
+    .blocklist_file(r#"(.*[/\\])?SDL_platform_defines.h"#)
+
+    // only used for static assertions
+    .blocklist_type("SDL_DUMMY_ENUM")
+
+    // va_list functions aren't really usable in Rust right now
+    .blocklist_type(".*va_list.*")
+    .blocklist_function("SDL_LogMessageV")
+    .blocklist_function(r#"SDL_v\w+(scanf|printf)"#)
+
+    // forward declaration used in SDL_main.h, not applicable to rust
+    .blocklist_function("SDL_main")
+
+    // SDL's doc comments are inconsistent and don't translate to
+    // rustdoc very well, causing warnings and build failures due to
+    // being misinterpreted as doctests. Use the official wiki instead.
+    .generate_comments(false)
+
+    // Used mostly for the hints API.
+    // More semantically correct and saves some typecasting.
+    .generate_cstr(true)
+
+    // SDL's enum naming is fairly good, so prepending the enum name just
+    // causes the identifiers to be way longer than they need to be.
+    .prepend_enum_name(false)
+
+    // Cleanliness and a vague attempt at generating diffable files.
+    .sort_semantically(true)
+
+    .clang_arg("-Iheaders")
+    .header("wrapper.h")
+    .generate()
+    .unwrap()
+    .write_to_file("bindings.rs")
+    .unwrap();
 }
--- sdl3-bindgen/wrapper.h
+++ sdl3-bindgen/wrapper.h
@@ -10,3 +10,16 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_syswm.h>
 #include <SDL3/SDL_vulkan.h>
+
+// SDL provides initialization code for several platforms in the form of redefining main(),
+// and calling SDL_RunApp from the platform's entry function (e.g. WinMain), which in turn
+// performs the necessary setup and calls SDL_SetMainReady followed by the user's main function.
+//
+// In Rust, this is a lot more complicated, because for example things like WinMain are already provided
+// by the language runtime (std). Maybe a proc macro in the spirit of something like #[tokio::main]
+// could handle this boilerplate for the platforms that need it, but at any rate, the functions should
+// be exposed by the bindings. The macro SDL_MAIN_HANDLED ensures that the header only provides declarations.
+//
+// Windows (non-WinRT/UWP), Linux, and MacOS at any rate do not need any special setup. Rust handles everything.
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL_main.h>
