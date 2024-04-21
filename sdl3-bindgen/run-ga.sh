a6e7e7c4ebdf5c4938a2399325c3611b6ccf9329
ShinySaana <shinysaana@gmail.com>
2024-04-21 13:13:20 +0200

    task(bindgen): Add multiplatform builds

--- sdl3-bindgen/run-ga.sh
+++ sdl3-bindgen/run-ga.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+set -e
+
 target=$1
 
-if [ "$target" = "x86_64-unknown-linux-gnu" ]; then
-    echo generating bindings_x86_64-unknown-linux-gnu.rs
-    cargo run -- bindings_x86_64-unknown-linux-gnu.rs
-elif [ "$target" = "x86_64-pc-windows-gnu" ]; then
-    echo generating bindings_x86_64-pc-windows-gnu.rs
-    BINDGEN_EXTRA_CLANG_ARGS='-target x86_64-pc-windows-gnu --sysroot /usr/x86_64-w64-mingw32' cargo run -- bindings_x86_64-pc-windows-gnu.rs
-elif [ "$target" = "x86_64-pc-windows-msvc" ]; then
-    echo generating bindings_x86_64-pc-windows-msvc.rs
-    cargo run -- bindings_x86_64-pc-windows-msvc.rs
-else
-    echo 'unsupported platform'
+if [ "$target" = "x86_64-pc-windows-gnu" ]; then
+    export BINDGEN_EXTRA_CLANG_ARGS='-target x86_64-pc-windows-gnu --sysroot /usr/x86_64-w64-mingw32'
 fi
+
+echo generating $target.rs
+cargo run -- bindings_$target.rs
