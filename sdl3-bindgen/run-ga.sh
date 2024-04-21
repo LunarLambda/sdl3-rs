f3085a892ad726a7f5fb077d85dee9801f25b30b
ShinySaana <shinysaana@gmail.com>
2024-04-21 16:32:12 +0200

    fix(bindgen): Fix untrue log statement

--- sdl3-bindgen/run-ga.sh
+++ sdl3-bindgen/run-ga.sh
@@ -8,5 +8,5 @@ if [ "$target" = "x86_64-pc-windows-gnu" ]; then
     export BINDGEN_EXTRA_CLANG_ARGS='-target x86_64-pc-windows-gnu --sysroot /usr/x86_64-w64-mingw32'
 fi
 
-echo generating $target.rs
+echo generating bindings_$target.rs
 cargo run -- bindings_$target.rs
