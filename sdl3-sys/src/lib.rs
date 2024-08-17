b223c31a7c5fca6eb013a90715a50765ffa6b2df
Luna Saphie Mittelbach <lunarlambda@gmail.com>
2024-08-17 11:07:18 +0200

    [wip, broken] bump sdl, use github-actions'd bindings

--- sdl3-sys/src/lib.rs
+++ sdl3-sys/src/lib.rs
@@ -3,13 +3,19 @@
 #![allow(non_snake_case)]
 
 cfg_if::cfg_if! {
-    if #[cfg(target_os = "linux")] {
+    if #[cfg(all(target_arch = "x86_64", target_os = "linux"))] {
+        #[path = "generated/bindings_x86_64-unknown-linux-gnu.rs/bindings_x86_64-unknown-linux-gnu.rs"]
         mod bindings_linux;
         pub use bindings_linux::*;
-    } else if #[cfg(target_os = "windows")] {
+    } else if #[cfg(all(target_arch = "x86_64", target_os = "windows"))] {
+        #[path = "generated/bindings_x86_64-pc-windows-msvc.rs/bindings_x86_64-pc-windows-msvc.rs"]
         mod bindings_windows;
         pub use bindings_windows::*;
+    } else if #[cfg(all(target_arch = "aarch64", target_os = "macos"))] {
+        #[path = "generated/bindings_aarch64-apple-darwin.rs/bindings_aarch64-apple-darwin.rs"]
+        mod bindings_macos;
+        pub use bindings_macos::*;
     } else {
-        compile_error!("no bindings available for the current platform")
+        compile_error!("This target is not supported");
     }
 }
