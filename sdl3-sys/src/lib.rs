2f9ca0a38b2fe885cb70db5ce5ee93c8a952d4d2
Luna Saphie Mittelbach <lunarlambda@gmail.com>
2024-08-17 11:33:08 +0200

    rearrange the bindings in a reasonable way

--- sdl3-sys/src/lib.rs
+++ sdl3-sys/src/lib.rs
@@ -2,20 +2,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
-cfg_if::cfg_if! {
-    if #[cfg(all(target_arch = "x86_64", target_os = "linux"))] {
-        #[path = "generated/bindings_x86_64-unknown-linux-gnu.rs/bindings_x86_64-unknown-linux-gnu.rs"]
-        mod bindings_linux;
-        pub use bindings_linux::*;
-    } else if #[cfg(all(target_arch = "x86_64", target_os = "windows"))] {
-        #[path = "generated/bindings_x86_64-pc-windows-msvc.rs/bindings_x86_64-pc-windows-msvc.rs"]
-        mod bindings_windows;
-        pub use bindings_windows::*;
-    } else if #[cfg(all(target_arch = "aarch64", target_os = "macos"))] {
-        #[path = "generated/bindings_aarch64-apple-darwin.rs/bindings_aarch64-apple-darwin.rs"]
-        mod bindings_macos;
-        pub use bindings_macos::*;
-    } else {
-        compile_error!("This target is not supported");
-    }
-}
+#[cfg_attr(all(target_arch = "x86_64", target_os = "linux"), path = "bindings/x86_64-unknown-linux-gnu.rs")]
+#[cfg_attr(all(target_arch = "x86_64", target_os = "windows"), path = "bindings/x86_64-pc-windows-msvc.rs")]
+#[cfg_attr(all(target_arch = "aarch64", target_os = "macos"), path = "bindings/aarch64-apple-darwin.rs")]
+mod bindings;
+pub use bindings::*;
