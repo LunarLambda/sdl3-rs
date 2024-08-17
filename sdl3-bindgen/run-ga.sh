16d152f3c948402095005335419e7878a6b4fbea
Luna Saphie Mittelbach <lunarlambda@gmail.com>
2024-08-17 11:15:49 +0200

    fix(actions): make script check that a target was passed

--- sdl3-bindgen/run-ga.sh
+++ sdl3-bindgen/run-ga.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-target=$1
+target="${1:?missing target}"
 
 if [ "$target" = "x86_64-pc-windows-gnu" ]; then
     export BINDGEN_EXTRA_CLANG_ARGS='-target x86_64-pc-windows-gnu --sysroot /usr/x86_64-w64-mingw32'
